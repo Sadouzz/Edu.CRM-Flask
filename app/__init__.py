212349f581fdb65e3491719172c5c82fac0b37ec
Jordan <156598745+Jordan-0705@users.noreply.github.com>
2026-03-07 13:05:19 +0000

    Tache 2 : __init__.py (Creation du Blueprint students )
    Initialize Flask application and register students blueprint.

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Importer les blueprints
+  
+    # from app.auth.route import auth_bp
+    from app.students.route import students_bp
+    # from app.teachers.route import teachers_bp
+    # from app.courses.route import courses_bp
+    # from app.dashboard.route import dashboard_bp
+
+    # Enregistrer les blueprints
+  
+    # app.register_blueprint(auth_bp)
+    app.register_blueprint(students_bp, url_prefix="/students")
+    # app.register_blueprint(teachers_bp, url_prefix="/teachers")
+    # app.register_blueprint(courses_bp, url_prefix="/courses")
+    # app.register_blueprint(dashboard_bp)
+
+    return app
